2d18bddaac12677cfe5dec1f07dcf90f4ff98beb
vidhisharma3193 <vidhisharma3193@gmail.com>
2021-04-07 14:20:10 -0500

    fix code

--- lecture_kit/app/models/person.rb
+++ lecture_kit/app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ActiveRecord::Base
 
     has_many :plant_parenthoods
     has_many :plants, through: :plant_parenthoods
+
+    has_many :waterings
+    has_many :waterees, through: :waterings
 end
